da0ed4db433b522f931dad818f4cea6d5f595144
Lukáš 'Radar' Kubíček <kubicek@radarsoft.cz>
2019-06-10 15:35:17 +0200

    adde /subscribe command

--- src/controllers/telegram/webhook-processor.ts
+++ src/controllers/telegram/webhook-processor.ts
@@ -1,7 +1,24 @@
 // import axios from 'axios'
+import fs from 'fs'
+import path from 'path'
+
 export const processWebhook = (req, res, next) => {
   const { botToken } = req.query
-  if (botToken === process.env.TG_BOT_TOKEN) {
+  const { from, text } = req.body
+  if (botToken === process.env.TG_BOT_TOKEN && !from['is_bot']) {
+    if (text.startsWith('/subscribe')) {
+      const subscribersFile = path.join(process.cwd(), 'data/subscribers.json')
+      if (!fs.existsSync(subscribersFile)) {
+        fs.writeFileSync(subscribersFile, '[]')
+      }
+
+      const rawData = fs.readFileSync(subscribersFile, 'utf8')
+      const jsonData: string[] = JSON.parse(rawData)
+
+      jsonData.push(`${from.id}`)
+
+      fs.writeFileSync(subscribersFile, jsonData)
+    }
     res.status(200).end()
   } else {
     res.status(401).end()
