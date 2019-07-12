c5cd975b1c8c1daa3860567c39bb0c0077311a10
Lukáš 'Radar' Kubíček <kubicek@radarsoft.cz>
2019-07-12 20:09:48 +0200

    added reposting ETS to discord and telegram

--- src/controllers/trucksbook/tb-webhook.ts
+++ src/controllers/trucksbook/tb-webhook.ts
@@ -3,10 +3,12 @@ import axios from 'axios'
 export const resendETS = (req, res, next) => {
     const requestData = JSON.parse(Object.keys(req.body)[0])
     const data = {
-        'chat_id': '414159721',
-        'text': requestData
+        'chat_id': `${process.env.TG_ETS_CHAT}`
     }
+    data['text'] = `*${requestData.username}* odjel zakázku:\n`
+    data['text'] += `${requestData.content}`
 
+    axios.post(`${process.env.TB_ETS_DISCORD}`, req.body)
     axios.post(`https://api.telegram.org/bot${process.env.TG_BOT_TOKEN}/sendMessage`, data)
     res.status(200).end()
 }
