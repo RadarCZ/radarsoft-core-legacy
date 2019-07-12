780c7a4b9e6e29ef96a0ee92d86c9fc160a710c6
Lukáš Kubíček <kubicek@radarsoft.cz>
2019-07-12 20:32:27 +0200

    finished ETS resending

--- src/controllers/trucksbook/tb-webhook.ts
+++ src/controllers/trucksbook/tb-webhook.ts
@@ -3,12 +3,13 @@ import axios from 'axios'
 export const resendETS = (req, res, next) => {
     const requestData = JSON.parse(Object.keys(req.body)[0])
     const data = {
-        'chat_id': `${process.env.TG_ETS_CHAT}`
+        'chat_id': `${process.env.TG_ETS_CHAT}`,
+        'parse_mode': 'Markdown'
     }
     data['text'] = `*${requestData.username}* odjel zakázku:\n`
     data['text'] += `${requestData.content}`
 
-    axios.post(`${process.env.TB_ETS_DISCORD}`, req.body)
+    axios.post(`${process.env.TB_ETS_DISCORD}`, requestData)
     axios.post(`https://api.telegram.org/bot${process.env.TG_BOT_TOKEN}/sendMessage`, data)
     res.status(200).end()
 }
