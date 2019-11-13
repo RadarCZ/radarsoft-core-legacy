d0a5cf64c83fd625cb81d1b11555025593f8b4d3
Lukáš 'Radar' Kubíček <kubicek@radarsoft.cz>
2019-11-13 20:32:18 +0100

    fix gc temporarily, until I find new workaround

--- src/controllers/contacts/geocaching-controller.ts
+++ src/controllers/contacts/geocaching-controller.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { parse } from 'node-html-parser'
 
 export const getGcData = async (req, res, next) => {
   const { u } = req.query
@@ -7,15 +6,12 @@ export const getGcData = async (req, res, next) => {
   const gcResponse = await axios.get(url)
 
   if (gcResponse.status === 200 || gcResponse.status === 300) {
-    const gcHtml: any = parse(gcResponse.data)
-    const stat1Text = gcHtml.querySelectorAll('.profile-stats')[0].querySelectorAll('li')[0].text.trim()
-    const stat2Text = gcHtml.querySelectorAll('.profile-stats')[0].querySelectorAll('li')[2].text.trim()
     const resultData = {
-      'icon': gcHtml.querySelectorAll('.profile-image')[0].attributes.src,
-      'name': gcHtml.querySelectorAll('h1')[0].text,
+      'icon': 'https://img.geocaching.com/user/square250/6632b799-e189-4ea1-9072-a2d48c03452f.png',
+      'name': u,
       'site': 'geocaching',
-      'stat1': stat1Text.substr(0, stat1Text.indexOf(' ')),
-      'stat2': stat2Text.substr(0, stat1Text.indexOf(' ')),
+      'stat1': 75,
+      'stat2': 5,
       'targetUrl': url
     }
 
