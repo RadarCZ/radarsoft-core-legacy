e6a83ddcda16465e4320efa8dc8a207a921d727b
Lukáš 'Radar' Kubíček <kubicek@radarsoft.cz>
2020-01-29 16:52:41 +0100

    1.7.11

--- src/twitch/handlers/commands/lol.ts
+++ src/twitch/handlers/commands/lol.ts
@@ -3,6 +3,6 @@ import TwitchClient from '../../TwitchClient'
 export default (channel: string) => {
     const client = TwitchClient.getInstance()
     const account = 'RadarCZ'
-    const server = 'EUNE'
+    const server = 'EUW'
     client.say(channel, `Jméno vyvolávače je ${account} a hraje na serveru ${server}`)
 }
