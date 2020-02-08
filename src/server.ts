752057fcaa32849c5e25c77f8f2e66fcb991c017
Jakub Rohla <freemanovec@protonmail.com>
2020-02-08 23:49:58 +0100

    Check for Telegram key before attaching webhook

--- src/server.ts
+++ src/server.ts
@@ -13,12 +13,16 @@ const server = app.listen(app.get('port'), () => {
   handlePost();
   post2019nCovUpdate();
 
-  axios.post(`https://api.telegram.org/bot${process.env.TG_BOT_TOKEN}/setWebhook`, {
-    'url' : `https://radarsoft.cz/api/telegram/processUpdate?botToken=${process.env.TG_BOT_TOKEN}`,
-    'allowed_updates' : ['message']
-  }).then(() => {
-    logger.info('Telegram WebHook endpoint set.');
-  }).catch(logger.info);
+  if (process.env.TG_BOT_TOKEN) {
+    axios.post(`https://api.telegram.org/bot${process.env.TG_BOT_TOKEN}/setWebhook`, {
+      'url' : `https://radarsoft.cz/api/telegram/processUpdate?botToken=${process.env.TG_BOT_TOKEN}`,
+      'allowed_updates' : ['message']
+    }).then(() => {
+      logger.info('Telegram WebHook endpoint set.');
+    }).catch(logger.info);
+  } else {
+    logger.warn('Unable to attach Telegram webhook, no token (TG_BOT_TOKEN)');
+  }
 
   if (!!process.env.TWITCH_BOT_USERNAME
     && process.env.TWITCH_BOT_OAUTH
